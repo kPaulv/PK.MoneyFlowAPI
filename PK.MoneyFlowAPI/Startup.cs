320cd7a06aa366b574ced10f2fcc79ce6d714f71
Paul <pavel.k.313@mail.ru>
2021-12-27 20:28:29 +0300

    implemented Swagger in Startup

--- PK.MoneyFlowAPI/Startup.cs
+++ PK.MoneyFlowAPI/Startup.cs
@@ -16,6 +16,8 @@ namespace PK.MoneyFlowAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore().AddApiExplorer();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +27,9 @@ namespace PK.MoneyFlowAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseRouting();
 
